1fb210ba803d9dfe95f47687f225a99c1302700d
icedjello <revues.counter0x@icloud.com>
2024-03-08 20:45:08 +0000

    imported contents

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,22 @@
-import { useState } from "react";
-import "./App.css";
+import { useEffect } from "react";
+// import "./App.css";
+import {
+  personalDetails,
+  skillsAndTechnologies,
+  employmentHistory,
+} from "./contents.json";
 
 function App() {
-  const [count, setCount] = useState(0);
+  useEffect(() => {
+    const pageTitle = document.querySelector("title") as HTMLTitleElement;
+    pageTitle.textContent = `${personalDetails.preferredName} ${personalDetails.lastName}`;
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          {/* <img src={viteLogo} className="logo" alt="Vite logo" /> */}
-        </a>
-        <a href="https://react.dev" target="_blank">
-          {/* <img src={reactLogo} className="logo react" alt="React logo" /> */}
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <pre>{JSON.stringify(personalDetails)}</pre>
+      <pre>{JSON.stringify(skillsAndTechnologies)}</pre>
+      <pre>{JSON.stringify(employmentHistory)}</pre>
     </>
   );
 }
